d0e12de49fec0b16e6c2b7cd716b5c07f3f338fd
Daniel Andreassi <danielandreassi1@gmail.com>
2024-11-09 18:11:17 -0300

    add Dependecies and create SecurityConfig

--- src/main/java/com/danielandreassi/todosimple/services/excepitions/DataBindingViolationException.java
+++ src/main/java/com/danielandreassi/todosimple/services/excepitions/DataBindingViolationException.java
@@ -4,8 +4,6 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-
-
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class DataBindingViolationException extends DataIntegrityViolationException {
 
